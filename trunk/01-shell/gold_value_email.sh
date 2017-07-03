f7a697d5dab2f87a0c010e7179a0e0e1ed5746a9
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-03 14:33:37 +0800

    changed log file name

--- trunk/01-shell/gold_value_email.sh
+++ trunk/01-shell/gold_value_email.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #test1
-v_logPath=/tmp/log_value.log
+v_logPath=/tmp/gold_value.log
 v_gold_now=`curl -i --get --include 'http://jisugold.market.alicloudapi.com/gold/shgold' -H 'Authorization:APPCODE 5ca26b23675f4b7f81b365d115184cdf'|awk -F "沪金99\",\"price\":\"" '{print $2}'|awk -F "\"" '{print $1}'|tail -1`
 
 echo `date +%Y-%m-%d" "%H:%M:%S`" $v_gold_now" >> $v_logPath
