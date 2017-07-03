52bd2e6f98fdc4f4f817c37cedab898ca07acfa2
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-03 14:14:15 +0800

    added comment

--- trunk/01-shell/gold_value_email.sh
+++ trunk/01-shell/gold_value_email.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#test
 v_logPath=/tmp/log_value.log
 v_gold_now=`curl -i --get --include 'http://jisugold.market.alicloudapi.com/gold/shgold' -H 'Authorization:APPCODE 5ca26b23675f4b7f81b365d115184cdf'|awk -F "沪金99\",\"price\":\"" '{print $2}'|awk -F "\"" '{print $1}'|tail -1`
 
