0f2fca33cca8f7158c826e1c39ca0ff38b3e0f85
Alvin Wan <alvin.wan.cn@hotmail.com>
2017-07-17 15:19:07 +0800

    Update tmp.sh

--- trunk/01-shell/tmp.sh
+++ trunk/01-shell/tmp.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-<<EOF
-function AA
-{
-read -p "Please input a number:" NUMBER
-}
-function BB
-{
-if [ $NUMBER == a ];then
-	echo "your input is a"
-	else
-	echo "what's your input?"
-fi
-}
-AA
-BB
-EOF
+
 echo "hello"
 echo "ok"
