5c4b49508ba89dd5abf8f6738a5d4f4a8dc3b7dc
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-11 11:17:54 +0800

    Update disableSeAndFir.sh

--- common_tools/disableSeAndFir.sh
+++ common_tools/disableSeAndFir.sh
@@ -1,5 +1,5 @@
  
- cm1="sed - 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config"
+ cm1="sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config"
  cm2="systemctl stop firewalld"
  cm3="systemctl disable firewalld"
  $cm1 && $cm2 && $cm3
