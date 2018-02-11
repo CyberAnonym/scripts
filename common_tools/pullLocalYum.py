a2081279f40a0742439821354db7f8187d1806f3
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-11 19:13:00 +0800

    1

--- common_tools/pullLocalYum.py
+++ common_tools/pullLocalYum.py
@@ -3,5 +3,6 @@ import os,re
 
 systemVersion=re.findall(r'\d',os.popen('cat /etc/redhat-release').read())[0]
 repoUrl='https://github.com/AlvinWanCN/TechnologyCenter/raw/master/linux/software/centos%s.dc.alv.pub.repo'%systemVersion
+print(repoUrl)
 os.system('curl -fsSL %s > /etc/yum.repos.d/centos%s.dc.alv.pub.repo'%(repoUrl,systemVersion))
 os.system('yum repolist')
