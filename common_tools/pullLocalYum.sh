81016c007fd8df977a84d1a29b7fa6112f4ca5e8
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-12 10:31:05 +0800

    Updated script content.

--- common_tools/pullLocalYum.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-systemVersion=$(python -c "import os,re;print(re.findall(r'\d',os.popen('cat /etc/redhat-release').read()))[0]")
-
-repoUrl=https://github.com/AlvinWanCN/TechnologyCenter/raw/master/linux/software/centos7.dc.alv.pub.repo
-
-curl -sSL $repoUrl > /etc/yum.repos.d/centos7.dc.alv.pub.repo
--- common_tools/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-#定义确认命令是否执行成功的消息输出
-
-import os
-os.system('ls')
-print('hello')
