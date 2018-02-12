b8d1e33067df623ecf23c56661b3e64cf2544875
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-12 10:29:29 +0800

    Updated script content.

--- common_tools/pullLocalYum.py
+++ common_tools/pullLocalYum.py
@@ -1,8 +1,28 @@
 #coding:utf-8
-import os,re
+import os,re,shutil
 
-systemVersion=re.findall(r'\d',os.popen('cat /etc/redhat-release').read())[0]
-repoUrl='https://github.com/AlvinWanCN/TechnologyCenter/raw/master/linux/software/centos%s.dc.alv.pub.repo'%systemVersion
-print(repoUrl)
-os.system('curl -fsSL %s > /etc/yum.repos.d/centos%s.dc.alv.pub.repo'%(repoUrl,systemVersion))
-os.system('yum repolist')
+def backupOriginRepo():
+    originDIR='/etc/yum.repos.d/'
+    backDIR='/etc/yum.repos.d/backup'
+    if os.path.exists(backDIR):
+        pass
+    else:
+        os.mkdir(backDIR)
+    files=os.listdir('/etc/yum.repos.d')
+    for file in files:
+        if re.findall(r'^Cent',file):
+            shutil.move(os.path.join(originDIR,file),os.path.join(backDIR,file))
+def getRepo():
+    systemVersion=re.findall(r'\d',os.popen('cat /etc/redhat-release').read())[0]
+    repoUrl='https://github.com/AlvinWanCN/TechnologyCenter/raw/master/linux/software/centos%s.dc.alv.pub.repo'%systemVersion
+    os.system('curl -fsSL %s > /etc/yum.repos.d/centos%s.dc.alv.pub.repo'%(repoUrl,systemVersion))
+    os.system('yum repolist')
+def main():
+    backupOriginRepo()
+    getRepo()
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print("Detected error.")
+        print(e)
--- common_tools/test.py
+++ common_tools/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+
 aa=111
 def aa():
     aa=222
 
 print(aa)
+os.path.dirname(__file__)
