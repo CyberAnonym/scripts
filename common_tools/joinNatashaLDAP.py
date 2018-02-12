19923d444fa8a96ba4b92d1e5d775f1eee0c516a
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-02-12 09:27:19 +0800

    Updated script content.

--- common_tools/joinNatashaLDAP.py
+++ common_tools/joinNatashaLDAP.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 #conding:utf-8
 import os,re
-openAutoMasterR=open('/etc/auto.master','r')
-openAutoSophiroth=open('/etc/auto.sophiroth','w')
 def op0(content): #op是out put简写定义绿色字体输出success 红色字体是31m.绿色字是32m# 黄色颜色是33m,
     print(content+'\033[032m [success] \033[0m')
 def op1(content):
@@ -19,6 +17,8 @@ def joinLDAP(): #加入ldap
 def installAutofs(): #安装autofs
     processCommand(os.system('yum -y install autofs nfs-utils'),"autofs has been installed","Failed install autofs")
 def configureAuthfs(): #配置autofs
+    openAutoMasterR = open('/etc/auto.master', 'r')
+    openAutoSophiroth = open('/etc/auto.sophiroth', 'w')
     if re.findall(r'sophiroth',openAutoMasterR.read()): #检查/etc/auto.master里是否已添加了sophiroth。
         openAutoMasterR.close()
     else: #如果没有添加，那么现在就添加
