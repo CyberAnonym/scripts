365aa9493698bd81a54d3e670669a256999eb1be
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-30 12:29:10 +0800

    change vmstat 1 1 to vmstat 1 2

--- python/instrallpython3.6.4.py
+++ python/instrallpython3.6.4.py
@@ -2,13 +2,13 @@
 #coding:utf-8
 import subprocess,os
 
-subprocess.call('yum install gcc zlib zlib-devel libffi-devel -y')
+subprocess.call('yum install gcc zlib zlib-devel libffi-devel -y',shell=True)
 os.chdir('/tmp')
-subprocess.call('curl -fsSL https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz > python3.6.5.tar.xz')
-subprocess.call('tar xf python3.6.5.tar.xz -C /usr/local/src/')
-os.chdir('/usr/local/src/Python-3.6.5/')
-subprocess.call('./configure --prefix=/usr/local/python3')
-subprocess.call('make')
-subprocess.call('make install')
-subprocess.call('ln -s /usr/local/python3/bin/python3 /usr/bin/')
-subprocess.call('python3 --version')
+subprocess.call('curl -fsSL https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz > python3.6.5.tar.xz',shell=True)
+subprocess.call('tar xf python3.6.5.tar.xz -C /usr/local/src/',shell=True)
+os.chdir('/usr/local/src/Python-3.6.5/',shell=True)
+subprocess.call('./configure --prefix=/usr/local/python3',shell=True)
+subprocess.call('make',shell=True)
+subprocess.call('make install',shell=True)
+subprocess.call('ln -s /usr/local/python3/bin/python3 /usr/bin/',shell=True)
+subprocess.call('python3 --version',shell=True)
