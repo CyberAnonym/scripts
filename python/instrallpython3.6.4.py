204cb86e6a6116a6d0dacc81373121c229355fc7
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-30 11:58:36 +0800

    change vmstat 1 1 to vmstat 1 2

---
+++ python/instrallpython3.6.4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#coding:utf-8
+import subprocess,os
+
+subprocess.call('yum install gcc zlib zlib-devel libffi-devel -y')
+os.chdir('/tmp')
+subprocess.call('curl -fsSL https://www.python.org/ftp/python/3.6.5/Python-3.6.4.tar.xz > python3.6.5.tar.xz')
+subprocess.call('tar xf python3.6.5.tar.xz -C /usr/local/src/')
+os.chdir('/usr/local/src/Python-3.6.5/')
+subprocess.call('./configure --prefix=/usr/local/python3')
+subprocess.call('make')
+subprocess.call('make install')
+subprocess.call('ln -s /usr/local/python3/bin/python3 /usr/bin/')
+subprocess.call('python3 --version')
