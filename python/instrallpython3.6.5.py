073485cef1f6b8faf356532c7b951e1e0bc9ac7c
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-30 12:56:41 +0800

    rename

--- python/instrallpython3.6.5.py
+++ python/instrallpython3.6.5.py
@@ -8,9 +8,13 @@ if os.path.exists('/usr/bin/python3'):
 
 subprocess.call('yum install gcc zlib zlib-devel libffi-devel -y',shell=True)
 os.chdir('/tmp')
-subprocess.call('curl -fsSL https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz > python3.6.5.tar.xz',shell=True)
+if os.path.exists('/tmp/python3.6.5.tar.xz'):
+    pass
+else:
+    subprocess.call('curl -fsSL https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz > python3.6.5.tar.xz',shell=True)
+
 subprocess.call('tar xf python3.6.5.tar.xz -C /usr/local/src/',shell=True)
-os.chdir('/usr/local/src/Python-3.6.5/',shell=True)
+os.chdir('/usr/local/src/Python-3.6.5/')
 subprocess.call('./configure --prefix=/usr/local/python3',shell=True)
 subprocess.call('make',shell=True)
 subprocess.call('make install',shell=True)
