41354a3768abbbda4d03732d25558d397275791e
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-07-02 15:21:54 +0800

    change natasha to ldap

--- common_tools/optimize_system.py
+++ common_tools/optimize_system.py
@@ -2,3 +2,4 @@
 import subprocess
 subprocess.call('echo "set ts=4" >> /etc/vimrc',shell=True)
 subprocess.call('echo "set expandtab" >> /etc/vimrc',shell=True)
+subprocess.call('ecgi "export HISTTIMEFORMAT=\"%F %T  \"" >> /etc/profile',shell=True)
